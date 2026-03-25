c5aa15009c3abd97475888c66e9fb07149d0ac18
Daniel Gil <dangilgut@gmail.com>
2026-03-25 13:39:39 +0100

    feat (core/dialog-confirmation/): creacion del componente

---
+++ src/core/dialog-confirmation/dialog-confirmation.component.ts
@@ -0,0 +1,9 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-confirmation',
+  imports: [],
+  templateUrl: './dialog-confirmation.component.html',
+  styleUrl:'./dialog-confirmation.component.scss'
+})
+export class DialogConfirmation { }
