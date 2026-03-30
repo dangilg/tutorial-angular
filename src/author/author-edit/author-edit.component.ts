10cbecd9a0a8746ff0ede2c498f09cd7c8e140a3
Daniel Gil <dangilgut@gmail.com>
2026-03-30 11:46:28 +0200

    feat: creacion del componente author-edit

---
+++ src/author/author-edit/author-edit.component.ts
@@ -0,0 +1,9 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-edit',
+  imports: [],
+  templateUrl: './author-edit.component.html',
+  styleUrl: './author-edit.component.css',
+})
+export class AuthorEditComponent { }
