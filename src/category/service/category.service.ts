00f17ab4e47235ce1acf1999b2dad83b8b4102cd
Daniel Gil <dangilgut@gmail.com>
2026-03-30 10:26:29 +0200

    feat: implementacion de la comunicacion con el backend

--- src/category/service/category.service.ts
+++ src/category/service/category.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from '../models/category';
@@ -7,23 +8,29 @@ import { CATEGORY_DATA } from '../models/mock-categories';
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  private baseUrl = 'http://localhost:8080/category';
 
   getCategories(): Observable<Category[]> {
-    return of(CATEGORY_DATA);
+    //return of(CATEGORY_DATA);
+    return this.http.get<Category[]>(this.baseUrl);
+
   }
 
 
   //<any> debería er <Category>
-   saveCategory(category: Category): Observable<any> {
-    //habrá que detectar cuando se está creando una nueva y cuando se está editando
-    //seguramnte en el backend, con una petición a la BD
-    console.log('src/category/service/category.service.ts [saveCategory]: guardado de categoria')
-    return of(null);
+   saveCategory(category: Category): Observable<Category> {
+    console.log(category);
+    const {id} =category;
+    const url = id? `${this.baseUrl}/${id}`:this.baseUrl;
+    return this.http.put<Category>(url,category);
+
   }
 
   deleteCategory(idCategory : number): Observable<any> {
-    console.log(`src/category/service/category.service.ts [deleteategory]: borrado de categoria con id ${idCategory}`)
-    return of(null);
+    return this.http.delete(`${this.baseUrl}/${idCategory}`);
   }
 }
