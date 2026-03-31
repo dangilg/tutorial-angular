44bc246c4de4ca1eae7a6677bd6709c9a0198735
Daniel Gil <dangilgut@gmail.com>
2026-03-31 10:47:17 +0200

    feat: creación del componente para sigIn

---
+++ src/signIn/signIn-modal/signIn-modal.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-modal',
+  imports: [],
+  templateUrl: './signIn-modal.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignInModalComponent { }
