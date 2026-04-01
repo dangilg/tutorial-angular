614b88add953da834f57b05085cf9cc6008bc8be
Daniel Gil <dangilgut@gmail.com>
2026-04-01 10:06:50 +0200

    feat: continuamos implementacion de SignInComponent

--- src/user/signIn-modal/signIn-modal.component.ts
+++ src/user/signIn-modal/signIn-modal.component.ts
@@ -1,22 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import { MatFormField, MatLabel } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
 import { User } from '../model/User';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignInService } from '../service/signIn.service';
 import { MatDialogRef } from '@angular/material/dialog';
+
+
 @Component({
   selector: 'app-sign-in-modal',
-  imports: [MatFormField, MatLabel, MatInput],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    ],
   templateUrl: './signIn-modal.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl:'./signIn-modal.component.scss',
 })
-export class SignInModalComponent {
+export class SignInModalComponent implements OnInit{
   user:User;
 
   constructor(
         public dialogRef: MatDialogRef<SignInModalComponent>,
-        private signInService: SignInService,
+        //private signInService: SignInService,
 
     ) {}
+
+   ngOnInit():void{
+
+    this.user = new User();
+   }
  }
